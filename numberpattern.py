ea1b25eb43f3a06494c7dcd5ca8330cc66ff74cb
sweta <52633654+sweta1609@users.noreply.github.com>
2021-12-29 12:36:16 +0530

    Create numberpattern.py

---
+++ numberpattern.py
@@ -0,0 +1,19 @@
+n = int (input())
+i = 1
+while i<=n:
+    t = 1
+    while t<=i:
+        print(t,end="")
+        t = t + 1
+    space = 1
+    while space<=(2*n) - (2*i):
+        print(' ',end="")
+        space = space + 1
+    p = i + 1- 1
+    j = 1
+    while j<=i:
+        print(p,end="")
+        j = j + 1
+        p = p - 1
+    print()
+    i = i + 1
