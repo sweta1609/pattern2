86f48b151996edc22b3557f6c4fc87af2b0593aa
sweta <52633654+sweta1609@users.noreply.github.com>
2021-12-29 12:35:02 +0530

    Create starpattern.py

---
+++ starpattern.py
@@ -0,0 +1,19 @@
+n = int (input())
+i = 1
+while i<= n:
+    spaces = 1
+    while  spaces <= n-i:
+        print (' ',end="")
+        spaces = spaces + 1
+    p = 1
+    j = 1
+    while j<=i:
+        print('*',end="")
+        j = j + 1
+        p = p + 1
+    p = i - 1
+    while p>=1:
+        print('*',end="")
+        p = p - 1
+    print()
+    i = i + 1
